fe2ad96628c40b362d0fbffd01aaf2645e0fafba
panda <duremarkastu@gmail.com>
2024-12-27 13:54:45 +0300

    update to board and board logic to not change set numbers during insert

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,30 @@
 
 int main() {
   int board[9][9] = {
-      {0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0},
-      {0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0},
-      {0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0},
-      {0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0},
-      {0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {9, 0, 3, 8, 0, 0, 0, 0, 2}, {0, 0, 8, 0, 3, 0, 1, 9, 5},
+      {6, 5, 0, 1, 0, 0, 0, 0, 7}, {7, 4, 0, 0, 6, 8, 0, 0, 3},
+      {0, 0, 5, 4, 9, 7, 6, 0, 0}, {0, 0, 9, 2, 0, 0, 0, 5, 0},
+      {0, 0, 0, 0, 2, 4, 8, 0, 0}, {0, 0, 7, 9, 5, 0, 0, 4, 6},
+      {1, 2, 4, 7, 8, 6, 0, 3, 9},
   };
 
   sudoku game;
 
+  // Cursir starting position
   int cursor[2] = {0, 0};
-  game.insertValue(board, 4, 5, 5);
-
-  // int x = game.verticalLinearSearch(board, 5);
 
   char input;
   int value;
+  bool isEditable[9][9];
 
+  // set cells as editable/changable only if value is 0 at the start of the game
+  for (int row = 0; row < 9; row++) {
+    for (int col = 0; col < 9; col++) {
+      isEditable[row][col] = (0 == board[row][col]);
+    }
+  }
+
+  // Main Game loop
   while (true) {
     system("clear"); // use "cls" for windows
 
@@ -29,6 +36,7 @@ int main() {
 
     game.displayBoard(board, cursor);
 
+    std::cout << "Choice: ";
     std::cin >> input;
 
     // quit the game
@@ -36,8 +44,16 @@ int main() {
       break;
     }
 
-    // inser value to the board
+    // inser value to the board based on the cursor's placement
     if ('i' == input) {
+      // Check if cell is editable
+      if (!isEditable[cursor[0]][cursor[1]]) {
+        std::cout << "Cannot Modify this cell!" << std::endl;
+        std::cin.ignore();
+        std::cin.get();
+        continue;
+      }
+
       std::cout << "Enter a value(1-9) to insert: ";
       std::cin >> value;
 
